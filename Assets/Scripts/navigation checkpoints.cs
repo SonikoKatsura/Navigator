2804bf34b04e399a9ed8a750752dbf4b6949a51c
Soniko <sonikonyas@gmail.com>
2023-12-19 22:49:10 +0000

    Added Jump Splines on navmesh links

--- Assets/Scripts/navigation checkpoints.cs
+++ Assets/Scripts/navigation checkpoints.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.AI.Navigation;
 using UnityEngine;
 using UnityEngine.AI;
 
@@ -9,9 +10,14 @@ public class navigationcheckpoints : MonoBehaviour
     int indexChildren;
     Vector3 destination;
 
+    private bool _onNavMeshLink = false;
+    [SerializeField] private float _jumpDuration = 0.8f;
+
     // Start is called before the first frame update
     void Start()
     {
+        GetComponent<NavMeshAgent>().autoTraverseOffMeshLink = false;
+
         destination = routeFather.GetChild(indexChildren).position;
         GetComponent<NavMeshAgent>().SetDestination(destination);
     }
@@ -27,5 +33,83 @@ public class navigationcheckpoints : MonoBehaviour
             destination = routeFather.GetChild(indexChildren).position;
             GetComponent <NavMeshAgent>().SetDestination(destination);
         }
+
+        if (GetComponent<NavMeshAgent>().isOnOffMeshLink && _onNavMeshLink == false)
+        {
+            StartNavMeshLinkMovement();
+        }
+        if (_onNavMeshLink)
+        {
+            FaceTarget(GetComponent<NavMeshAgent>().currentOffMeshLinkData.endPos);
+        }
+    }
+
+
+    private void StartNavMeshLinkMovement()
+    {
+        _onNavMeshLink = true;
+        NavMeshLink link = (NavMeshLink)GetComponent<NavMeshAgent>().navMeshOwner;
+        Spline spline = link.GetComponentInChildren<Spline>();
+
+        PerformJump(link, spline);
+    }
+
+    private void PerformJump(NavMeshLink link, Spline spline)
+    {
+        bool reverseDirection = CheckIfJumpingFromEndToStart(link);
+        StartCoroutine(MoveOnOffMeshLink(spline, reverseDirection));
+
+    }
+
+    private bool CheckIfJumpingFromEndToStart(NavMeshLink link)
+    {
+        Vector3 startPosWorld
+            = link.gameObject.transform.TransformPoint(link.startPoint);
+        Vector3 endPosWorld
+            = link.gameObject.transform.TransformPoint(link.endPoint);
+
+        float distancePlayerToStart
+            = Vector3.Distance(GetComponent<NavMeshAgent>().transform.position, startPosWorld);
+        float distancePlayerToEnd
+            = Vector3.Distance(GetComponent<NavMeshAgent>().transform.position, endPosWorld);
+
+
+        return distancePlayerToStart > distancePlayerToEnd;
+    }
+
+    private IEnumerator MoveOnOffMeshLink(Spline spline, bool reverseDirection)
+    {
+        float currentTime = 0;
+        Vector3 agentStartPosition = GetComponent<NavMeshAgent>().transform.position;
+
+        while (currentTime < _jumpDuration)
+        {
+            currentTime += Time.deltaTime;
+
+            float amount = Mathf.Clamp01(currentTime / _jumpDuration);
+            amount = reverseDirection ? 1 - amount : amount;
+
+            GetComponent<NavMeshAgent>().transform.position =
+                reverseDirection ?
+                spline.CalculatePositionCustomEnd(amount, agentStartPosition)
+                : spline.CalculatePositionCustomStart(amount, agentStartPosition);
+
+            yield return new WaitForEndOfFrame();
+        }
+
+        GetComponent<NavMeshAgent>().CompleteOffMeshLink();
+
+        yield return new WaitForSeconds(0.1f);
+        _onNavMeshLink = false;
+
+    }
+
+    void FaceTarget(Vector3 target)
+    {
+        Vector3 direction = (target - transform.position).normalized;
+        Quaternion lookRotation
+            = Quaternion.LookRotation(new Vector3(direction.x, direction.y, direction.z));
+        transform.rotation
+            = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * 5);
     }
 }
