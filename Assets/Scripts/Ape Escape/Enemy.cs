0bfffcfaad21fce8a5edf3eae0e4e55f6ec67911
Soniko <sonikonyas@gmail.com>
2024-02-19 18:25:17 +0000

    Working Project

--- Assets/Scripts/Ape Escape/Enemy.cs
+++ Assets/Scripts/Ape Escape/Enemy.cs
@@ -1,181 +1,20 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
-using Unity.AI.Navigation;
-using Unity.VisualScripting;
 using UnityEngine;
-using UnityEngine.AI;
-using Random = UnityEngine.Random;
 
 public class Enemy : MonoBehaviour
 {
-    [SerializeField] Vector3 min, max;
-    Vector3 destination;
+    [SerializeField]
+    private EnemyPatrol _input;
+    [SerializeField]
+    private AgentAnimation _agentAnimation;
 
-    private bool _onNavMeshLink = false;
-    [SerializeField] private float _jumpDuration = 0.8f;
-    private Transform player;
-    [SerializeField] float playerDistanceDetection;
-    [SerializeField] float visionAngle;
-    [SerializeField] float playerAttackDistance;
 
-    // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        player = GameObject.FindGameObjectWithTag("Player").transform;
-        RandomDestination();
-        StartCoroutine("Patrol");
-        StartCoroutine("Alert");
+        _input.OnSpeedChanged += _agentAnimation.SetSpeed;
+        _input.OnStartJump.AddListener(_agentAnimation.Jump);
+        _agentAnimation.SetSpeed(0);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-
-        if (GetComponent<NavMeshAgent>().isOnOffMeshLink && _onNavMeshLink == false)
-        {
-            StartNavMeshLinkMovement();
-        }
-        if (_onNavMeshLink)
-        {
-            FaceTarget(GetComponent<NavMeshAgent>().currentOffMeshLinkData.endPos);
-        }
-
-    }
-
-    public void RandomDestination()
-    {
-        destination = new Vector3(Random.Range(min.x, max.x), 0, Random.Range(min.z, max.z));
-        GetComponent<NavMeshAgent>().SetDestination(destination);
-        float speed = GetComponent<NavMeshAgent>().velocity.magnitude;
-        GetComponent<Animator>().SetFloat("vel", speed);
-    }
-
-    IEnumerator Patrol()
-    {
-        while (true)
-        {
-            if (Vector3.Distance(transform.position, destination) < 1.5f) 
-            {
-               
-                GetComponent<Animator>().SetFloat("vel", 0);
-                yield return new WaitForSeconds(Random.Range(1f, 3f));
-                RandomDestination();
-            }
-            yield return new WaitForEndOfFrame();
-        }
-        
-    }
-
-    IEnumerator Alert()
-    {
-        while (true)
-        {
-            if (Vector3.Distance(transform.position, player.position) < playerDistanceDetection)
-            {
-                Vector3 vectorPlayer = player.position - transform.position;
-                if (Vector3.Angle(vectorPlayer.normalized, transform.forward) < visionAngle)
-                { 
-                    StopCoroutine("Patrol");
-                    StartCoroutine("Attack");
-                    break;
-                }
-            }
-        }
-        yield return new WaitForEndOfFrame() ;
-    }
-
-    IEnumerator Attack() 
-    {
-        StopCoroutine("Alert");
-        while (true) 
-        {
-            if (Vector3.Distance(transform.position, player.position) < playerDistanceDetection) 
-            {
-                StartCoroutine("Patrol");
-                StartCoroutine("Alert");
-                StopCoroutine("Attack");
-
-            }
-            if (Vector3.Distance(transform.position, player.position) < playerAttackDistance)
-            {
-                GetComponent<NavMeshAgent>().SetDestination(transform.position);
-                GetComponent<NavMeshAgent>().velocity = Vector3.zero;
-                GetComponent<Animator>().SetTrigger("attack");
-            }
-            else { GetComponent<NavMeshAgent>().SetDestination(transform.position); }
-            yield return new WaitForEndOfFrame();
-        }
-        
-    }   
-
-    private void StartNavMeshLinkMovement()
-    {
-        _onNavMeshLink = true;
-        NavMeshLink link = (NavMeshLink)GetComponent<NavMeshAgent>().navMeshOwner;
-        Spline spline = link.GetComponentInChildren<Spline>();
-
-        PerformJump(link, spline);
-    }
-
-    private void PerformJump(NavMeshLink link, Spline spline)
-    {
-        GetComponent<Animator>().SetTrigger("jump");
-        bool reverseDirection = CheckIfJumpingFromEndToStart(link);
-        StartCoroutine(MoveOnOffMeshLink(spline, reverseDirection));
-
-    }
-
-    private bool CheckIfJumpingFromEndToStart(NavMeshLink link)
-    {
-        Vector3 startPosWorld
-            = link.gameObject.transform.TransformPoint(link.startPoint);
-        Vector3 endPosWorld
-            = link.gameObject.transform.TransformPoint(link.endPoint);
-
-        float distancePlayerToStart
-            = Vector3.Distance(GetComponent<NavMeshAgent>().transform.position, startPosWorld);
-        float distancePlayerToEnd
-            = Vector3.Distance(GetComponent<NavMeshAgent>().transform.position, endPosWorld);
-
-
-        return distancePlayerToStart > distancePlayerToEnd;
-    }
-
-    private IEnumerator MoveOnOffMeshLink(Spline spline, bool reverseDirection)
-    {
-        float currentTime = 0;
-        Vector3 agentStartPosition = GetComponent<NavMeshAgent>().transform.position;
-
-        while (currentTime < _jumpDuration)
-        {
-            currentTime += Time.deltaTime;
-
-            float amount = Mathf.Clamp01(currentTime / _jumpDuration);
-            amount = reverseDirection ? 1 - amount : amount;
-
-            GetComponent<NavMeshAgent>().transform.position =
-                reverseDirection ?
-                spline.CalculatePositionCustomEnd(amount, agentStartPosition)
-                : spline.CalculatePositionCustomStart(amount, agentStartPosition);
-
-            yield return new WaitForEndOfFrame();
-        }
-
-        GetComponent<NavMeshAgent>().CompleteOffMeshLink();
-
-        yield return new WaitForSeconds(0.1f);
-        _onNavMeshLink = false;
-
-    }
-
-    void FaceTarget(Vector3 target)
-    {
-        Vector3 direction = (target - transform.position).normalized;
-        Quaternion lookRotation
-            = Quaternion.LookRotation(new Vector3(direction.x, direction.y, direction.z));
-        transform.rotation
-            = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * 5);
-    }
 }
--- Assets/Scripts/Ape Escape/EnemyPatrol.cs
+++ Assets/Scripts/Ape Escape/EnemyPatrol.cs
@@ -1,74 +1,301 @@
 using System;
 using System.Collections;
-using System.Collections.Generic;
+using Unity.AI.Navigation;
 using UnityEditor.PackageManager;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.Events;
 using Random = UnityEngine.Random;
 
-public class EnemyPatrol : MonoBehaviour
+public class EnemyPatrol: MonoBehaviour
 {
-    [SerializeField] Vector3 min, max, destination;
-    public event Action<Vector3> OnGetDestination;
-    [SerializeField] Transform player;
-    [SerializeField] float playerDistanceDetection;
-    private float visionangle;
+    [SerializeField] Vector3 min, max;
+    Vector3 destination;
+    [SerializeField] float playerDetectionDistance, playerAttackDistance;
+    Transform player;
+    [SerializeField] float visionAngle;
+   
+   [SerializeField]
+    private NavMeshAgent _Agent;
 
-    private void Start()
+    public event Action<float> OnSpeedChanged;
+
+    private bool _onNavMeshLink = false;
+    private Mesh _NavMesh;
+    [SerializeField]
+    private float _jumpDuration = 0.8f;
+    public event Action<Vector3> DestinationGet;
+    public UnityEvent OnLand, OnStartJump;
+
+    void Start()
     {
+        NavMeshTriangulation triangles = NavMesh.CalculateTriangulation();
+        Mesh mesh = new Mesh();
+        _NavMesh = mesh;
+        _NavMesh.vertices = triangles.vertices;
+        _NavMesh.triangles = triangles.indices;
+        
+
+        _Agent.autoTraverseOffMeshLink = false;
         player = GameObject.FindGameObjectWithTag("Player").transform;
-        RandomDestination(); 
-        StartCoroutine("Patrol");
-        StartCoroutine("Alert");
-
-
+        RandomDestination();
+        StartCoroutine(Patrol());
+        StartCoroutine(Alert());
     }
-    public Vector3 RandomDestination()
+
+    void Update()
     {
-        destination = new Vector3(Random.Range(min.x, max.x), 0, Random.Range(min.z, max.z));
-        GetComponent<NavMeshAgent>().SetDestination(destination);
-        return destination;
        
     }
 
-    IEnumerator Patrol() 
+    void RandomDestination()
     {
-        while (true) 
+        destination = GetRandomPointOnMesh(_NavMesh);
+        GetComponent<NavMeshAgent>().SetDestination(destination);
+        GetComponent<Animator>().SetFloat("vel", 2);
+        DestinationGet?.Invoke(destination);
+    }
+    Vector3 GetRandomPointOnMesh(Mesh mesh)
+    {
+        //if you're repeatedly doing this on a single mesh, you'll likely want to cache cumulativeSizes and total
+        float[] sizes = GetTriSizes(mesh.triangles, mesh.vertices);
+        float[] cumulativeSizes = new float[sizes.Length];
+        float total = 0;
+
+        for (int i = 0; i < sizes.Length; i++)
         {
-            if (Vector3.Distance(transform.position, destination) < 1.5f)
+            total += sizes[i];
+            cumulativeSizes[i] = total;
+        }
+
+        //so everything above this point wants to be factored out
+
+        float randomsample = Random.value * total;
+
+        int triIndex = -1;
+
+        for (int i = 0; i < sizes.Length; i++)
+        {
+            if (randomsample <= cumulativeSizes[i])
             {
-                GetComponent<Animator>().SetFloat("vel", 0);
-                yield return new WaitForSeconds(Random.Range(1f, 3f));
-                RandomDestination();
-                OnGetDestination?.Invoke(destination);
-                Debug.Log($"Selected position is {destination}");
+                triIndex = i;
+                break;
             }
         }
-    }
-    IEnumerator Alert()
-    {
-        while(true)
-        {
-            if (Vector3.Distance(transform.position, player.position) < playerDistanceDetection)
-            {
-               Vector3 vectorPlayer = player.position  - transform.position;
-                if (Vector3.Angle(vectorPlayer.normalized, transform.forward) < visionangle)
-                {
-                    StopCoroutine("Patrol");
-                    GetComponent<NavMeshAgent>().SetDestination(player.position);
 
-                }
-                else 
-                {
-                    StartCoroutine("Patrol");
-                }
-            }
-            else 
+        if (triIndex == -1) Debug.LogError("triIndex should never be -1");
+
+        Vector3 a = mesh.vertices[mesh.triangles[triIndex * 3]];
+        Vector3 b = mesh.vertices[mesh.triangles[triIndex * 3 + 1]];
+        Vector3 c = mesh.vertices[mesh.triangles[triIndex * 3 + 2]];
+
+        //generate random barycentric coordinates
+
+        float r = Random.value;
+        float s = Random.value;
+
+        if (r + s >= 1)
+        {
+            r = 1 - r;
+            s = 1 - s;
+        }
+        //and then turn them back to a Vector3
+        Vector3 pointOnMesh = a + r * (b - a) + s * (c - a);
+        Debug.Log(pointOnMesh);
+        return pointOnMesh;
+  
+
+    }
+
+    float[] GetTriSizes(int[] tris, Vector3[] verts)
+    {
+        int triCount = tris.Length / 3;
+        float[] sizes = new float[triCount];
+        for (int i = 0; i < triCount; i++)
+        {
+            sizes[i] = .5f * Vector3.Cross(verts[tris[i * 3 + 1]] - verts[tris[i * 3]], verts[tris[i * 3 + 2]] - verts[tris[i * 3]]).magnitude;
+        }
+        return sizes;
+
+        /*
+         * 
+         * more readably:
+         * 
+for(int ii = 0 ; ii < indices.Length; ii+=3)
+{
+    Vector3 A = Points[indices[ii]];
+    Vector3 B = Points[indices[ii+1]];
+    Vector3 C = Points[indices[ii+2]];
+    Vector3 V = Vector3.Cross(A-B, A-C);
+    Area += V.magnitude * 0.5f;
+}
+         * 
+         * 
+         * */
+    }
+
+
+IEnumerator Patrol()
+    {
+        GetComponent<NavMeshAgent>().SetDestination(destination);
+        while (true)
+        {
+            OnSpeedChanged?.Invoke(
+                   Mathf.Clamp01(_Agent.velocity.magnitude / _Agent.speed));
+
+            if (_Agent.isOnOffMeshLink && _onNavMeshLink == false)
             {
-                StartCoroutine("Patrol");
+                StartNavMeshLinkMovement();
+            }
+            if (_onNavMeshLink)
+            {
+                FaceTarget(_Agent.currentOffMeshLinkData.endPos);
+            }
+            if (Vector3.Distance(transform.position, destination) < 1.5f)
+            {
+                GetComponent<Animator>().SetFloat("velocity", 0);
+                yield return new WaitForSeconds(Random.Range(1f, 3f));
+                RandomDestination();
             }
             yield return new WaitForEndOfFrame();
         }
-       
     }
+
+    IEnumerator Alert()
+    {
+        while (true)
+        {
+            OnSpeedChanged?.Invoke(
+                   Mathf.Clamp01(_Agent.velocity.magnitude / _Agent.speed));
+
+            if (_Agent.isOnOffMeshLink && _onNavMeshLink == false)
+            {
+                StartNavMeshLinkMovement();
+            }
+            if (_onNavMeshLink)
+            {
+                FaceTarget(_Agent.currentOffMeshLinkData.endPos);
+            }
+            if (Vector3.Distance(transform.position, player.position) < playerDetectionDistance)
+            {
+                Vector3 vectorPlayer = player.position - transform.position;
+                if (Vector3.Angle(vectorPlayer.normalized, transform.forward) < visionAngle)
+                {
+                    Debug.Log("Personaje detectado");
+                    StopCoroutine(Patrol());
+                    StartCoroutine(Attack());
+                    break;
+                }
+            }
+            yield return new WaitForEndOfFrame();
+        }
+    }
+
+    IEnumerator Attack()
+    {
+        while (true)
+        {
+            OnSpeedChanged?.Invoke(
+                    Mathf.Clamp01(_Agent.velocity.magnitude / _Agent.speed));
+
+            if (_Agent.isOnOffMeshLink && _onNavMeshLink == false)
+            {
+                StartNavMeshLinkMovement();
+            }
+            if (_onNavMeshLink)
+            {
+                FaceTarget(_Agent.currentOffMeshLinkData.endPos);
+            }
+            if (Vector3.Distance(transform.position, player.position) > playerDetectionDistance)
+            {
+                StartCoroutine(Patrol());
+                StartCoroutine(Alert());
+                break;
+            }
+            if (Vector3.Distance(transform.position, player.position) < playerAttackDistance)
+            {
+                GetComponent<NavMeshAgent>().SetDestination(transform.position);
+                GetComponent<NavMeshAgent>().velocity = Vector3.zero;
+                GetComponent<Animator>().SetBool("attack", true);
+                yield return new WaitForSeconds(3);
+            }
+            else
+            {
+                GetComponent<NavMeshAgent>().SetDestination(player.position);
+                GetComponent<Animator>().SetBool("attack", false);
+            }
+            yield return new WaitForEndOfFrame();
+        }
+    }
+    private void StartNavMeshLinkMovement()
+    {
+        _onNavMeshLink = true;
+        NavMeshLink link = (NavMeshLink)_Agent.navMeshOwner;
+        Spline spline = link.GetComponentInChildren<Spline>();
+
+        PerformJump(link, spline);
+    }
+
+    private void PerformJump(NavMeshLink link, Spline spline)
+    {
+        bool reverseDirection = CheckIfJumpingFromEndToStart(link);
+        StartCoroutine(MoveOnOffMeshLink(spline, reverseDirection));
+
+        OnStartJump?.Invoke();
+    }
+
+    private bool CheckIfJumpingFromEndToStart(NavMeshLink link)
+    {
+        Vector3 startPosWorld
+            = link.gameObject.transform.TransformPoint(link.startPoint);
+        Vector3 endPosWorld
+            = link.gameObject.transform.TransformPoint(link.endPoint);
+
+        float distancePlayerToStart
+            = Vector3.Distance(_Agent.transform.position, startPosWorld);
+        float distancePlayerToEnd
+            = Vector3.Distance(_Agent.transform.position, endPosWorld);
+
+
+        return distancePlayerToStart > distancePlayerToEnd;
+    }
+
+    private IEnumerator MoveOnOffMeshLink(Spline spline, bool reverseDirection)
+    {
+        float currentTime = 0;
+        Vector3 agentStartPosition = _Agent.transform.position;
+
+        while (currentTime < _jumpDuration)
+        {
+            currentTime += Time.deltaTime;
+
+            float amount = Mathf.Clamp01(currentTime / _jumpDuration);
+            amount = reverseDirection ? 1 - amount : amount;
+
+            _Agent.transform.position =
+                reverseDirection ?
+                spline.CalculatePositionCustomEnd(amount, agentStartPosition)
+                : spline.CalculatePositionCustomStart(amount, agentStartPosition);
+
+            yield return new WaitForEndOfFrame();
+        }
+
+        _Agent.CompleteOffMeshLink();
+
+        OnLand?.Invoke();
+        yield return new WaitForSeconds(0.1f);
+        _onNavMeshLink = false;
+
+    }
+
+
+    void FaceTarget(Vector3 target)
+    {
+        Vector3 direction = (target - transform.position).normalized;
+        Quaternion lookRotation
+            = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.z));
+        transform.rotation
+            = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * 5);
+    }
+
 }
